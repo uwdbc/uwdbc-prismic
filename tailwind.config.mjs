85823793d39bc6619471cb84c5e64a699704dcef
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-02-13 21:47:43 -0500

    added fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,10 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        body: ["var(--font-open-sans)"],
+        display: ["var(--font-inter)"]
+      }
     },
   },
   plugins: [],
