4d7532ceab57b1e1979aa34d2834e58908cc4c15
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-03-05 16:29:57 -0500

    continue to style footer

--- src/slices/Footer/index.js
+++ src/slices/Footer/index.js
@@ -1,3 +1,5 @@
+import { PrismicRichText } from "@prismicio/react";
+
 /**
  * @typedef {import("@prismicio/client").Content.FooterSlice} FooterSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<FooterSlice>} FooterProps
@@ -5,12 +7,35 @@
  */
 const Footer = ({ slice }) => {
   return (
-    <section
+    <footer
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      style={{backgroundImage:`url(${slice.primary.background_image.url})`}}
+      className="contact"
+      id="contact-us"
     >
-      Placeholder component for footer (variation: {slice.variation}) Slices
-    </section>
+      <div className="overlay"></div>
+      <div className="shadow-overlay"></div>
+      <div className="long-stick"></div>
+
+      <div className="contact-text">
+        <span>Contact Us</span>
+        <PrismicRichText field={slice.primary.small_paragraph} />
+      </div>
+
+      <div className="left-sec">
+        <div>
+          <h6>Where To Find Us</h6>
+          <PrismicRichText field={slice.primary.location} />
+        </div>
+
+        <div>
+          <h6>Email Us At</h6>
+          <a href={`mailto:${slice.primary.email}`}>{slice.primary.email}</a>
+        </div>
+      </div>
+
+    </footer>
   );
 };
 
