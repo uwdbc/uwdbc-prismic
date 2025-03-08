7fe3866bc4618594a5d196128da3be22f90b8a36
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-03-07 23:15:32 -0500

    styled and finished contact

--- src/slices/Footer/index.js
+++ src/slices/Footer/index.js
@@ -1,11 +1,31 @@
 import { PrismicRichText } from "@prismicio/react";
+import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+import { createClient } from "@/prismicio";
+
+import Discord from "/public/Discord";
+import Instagram from "/public/Instagram";
+import Linktree from "/public/Linktree";
+import Membership from "/public/Membership";
+import Email from "/public/Email";
+
+const social_linker = {
+  "Discord": <Discord />,
+  "Instagram": <Instagram />,
+  "Linktree": <Linktree />,
+  "Membership": <Membership />,
+  "Email": <Email />
+}
 
 /**
  * @typedef {import("@prismicio/client").Content.FooterSlice} FooterSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<FooterSlice>} FooterProps
  * @param {FooterProps}
  */
-const Footer = ({ slice }) => {
+const Footer = async ({ slice }) => {
+  const client = createClient();
+  const page = await client.getSingle("settings");
+  const data = page.data;
+
   return (
     <footer
       data-slice-type={slice.slice_type}
@@ -34,6 +54,18 @@ const Footer = ({ slice }) => {
           <a href={`mailto:${slice.primary.email}`}>{slice.primary.email}</a>
         </div>
       </div>
+
+      <div className="socials">
+        {slice.primary.links_to_social && slice.primary.links_to_social.map((item, index) => (
+          <PrismicNextLink field={item.link} key={index} className="social">
+            <div className="social-icon">{social_linker[item.link_type]}</div>  
+          </PrismicNextLink>
+        ))}
+      </div>
+
+      <a href="/">
+        <PrismicNextImage field={data.website_logo} className="logo"/>
+      </a>
 
     </footer>
   );
