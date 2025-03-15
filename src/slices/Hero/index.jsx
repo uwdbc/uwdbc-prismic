5397d3b8bb5abe75ae661ae7a605e524d15619a5
JeffGuo-06 <jeffguo.06@gmail.com>
2025-03-14 21:19:32 -0400

    team page first draft

--- src/slices/Hero/index.jsx
+++ src/slices/Hero/index.jsx
@@ -13,48 +13,48 @@ import Email from "../../../public/Email";
  */
 
 const components = {
-  heading1: ({children}) => (
-    <h1 className="s-title">{children}</h1>
-  )
-}
+  heading1: ({ children }) => <h1 className="s-title">{children}</h1>,
+};
 
 const social_linker = {
-  "Discord": <Discord />,
-  "Instagram": <Instagram />,
-  "Linktree": <Linktree />,
-  "Membership": <Membership />,
-  "Email": <Email />
-}
+  Discord: <Discord />,
+  Instagram: <Instagram />,
+  Linktree: <Linktree />,
+  Membership: <Membership />,
+  Email: <Email />,
+};
 
 const Hero = ({ slice }) => {
-
-  
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="s-hero"
-      style={{backgroundImage:`url(${slice.primary.background_image.url})`}}
+      style={{ backgroundImage: `url(${slice.primary.background_image.url})` }}
     >
-
       <div className="overlay"></div>
       <div className="shadow-overlay"></div>
 
       <div className="home-content">
         <PrismicRichText field={slice.primary.header} components={components} />
         <div className="btn-container">
-          <a href="#about" className="btn">About Us</a>
-          <a href="#contact-us" className="btn">Contact Us</a>
+          <a href="#about" className="btn">
+            About Us
+          </a>
+          <a href="#contact-us" className="btn">
+            Contact Us
+          </a>
         </div>
       </div>
 
       <div className="socials">
-        {slice.primary.links_to_social && slice.primary.links_to_social.map((item, index) => (
-          <PrismicNextLink field={item.link} key={index} className="social">
-            <p className="link-tag">{item.link_type}</p>
-            <div className="social-icon">{social_linker[item.link_type]}</div>  
-          </PrismicNextLink>
-        ))}
+        {slice.primary.links_to_social &&
+          slice.primary.links_to_social.map((item, index) => (
+            <PrismicNextLink field={item.link} key={index} className="social">
+              <p className="link-tag">{item.link_type}</p>
+              <div className="social-icon">{social_linker[item.link_type]}</div>
+            </PrismicNextLink>
+          ))}
       </div>
       <section className="bottom-hero">
         <div className="slider"></div>
